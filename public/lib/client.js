d9a125668bad0a0b74995528e65c0e87c9663027
Dominique Fober <fober@grame.fr>
2022-01-23 10:00:36 +0100

    clients synchronization using timesync and a static max latency strategy

--- public/lib/client.js
+++ public/lib/client.js
@@ -1,93 +1,108 @@
 
-var gDiv = null;
-var gSound = null;
-var gPartNum = 0;
-var gNetMaxLattency = 0;
-var gLocalLattency = 0;
+var gVideo = null;      // the video div
+var gSound = null;      // the sound div
+var gPartNum = 0;       // the part number - used to notify the conductor
+var gVideoLatency = 0;
+var gSoundLatency = 0;
 var gReady = false;
 
+//var gTimeOffset = 0;
+// gTime.on('change', function (offset) { gTimeOffset = offset; console.log ("Time offset:", offset)});
+
 function connectdiv (video, sound, partnum) {
-    gDiv = video;
+    gVideo = video;
     gSound = sound;
     gPartNum = partnum;
     connect();
     window.addEventListener('unload', function(event) { wsSend ('BYE ' + gPartNum); });
+    gVideo.addEventListener('play', () => { gSound.play() });
+    gVideo.addEventListener('pause', () => { gSound.pause() });
+    gVideo.addEventListener('seeked', () => { gSound.currentTime = gVideo.currentTime });
+    gVideo.addEventListener('volumechange', () => { gSound.volume = gVideo.volume });
+    gVideo.addEventListener('playing', videoLatency);
+    gSound.addEventListener('playing', soundLatency);
 }
 
 wsclient = function (data) {
-    console.log ("client receive", data);
-    if (!gDiv) {
+    if (!gVideo) {
         console.error ("Warning: incorrect initialization, div is undefined")
         return;
     }
-    switch (data) {
-        case 'PLAY': 
-            setTimeout( function(){ gDiv.play(); }, getDelay());
-            gSound.play();
-            break;
-        case 'PAUSE': 
-            gDiv.pause();
-            gSound.pause();
-            break;
-        case 'STOP': 
-            gDiv.pause();
-            gSound.pause();
-            gDiv.currentTime = 0;
-            gSound.currentTime = 0;
-            break;
-        case 'SYNC':                // request the local play latyency evaluation
-            getLocalLatency();
-            break;
-        case 'READY':               // sent by the conductor on startup
-            if (gReady) wsSend('PART ' + gPartNum);
-            break;        
-        default: 
-            rcvOther (data);
-            break;
-    }
-}
-
-function rcvOther(data) {
     const parts = data.split(' ');
     switch (parts[0]) {
+        case 'PLAY': 
+            console.log ("client receive", data);
+            playAt (parts[1]);
+            break;
+        case 'PAUSE': 
+            console.log ("client receive", data);
+            setTimeout( function(){ gVideo.pause(); gSound.pause() }, getDelay(parts[1]));
+            break;
+        case 'STOP': 
+            console.log ("client receive", data);
+            gVideo.pause();
+            gSound.pause();
+            gVideo.currentTime = 0;
+            gSound.currentTime = 0;
+            break;
+        case 'READY':               // sent by the conductor on startup
+            console.log ("client receive", data);
+            if (gReady) wsSend('PART ' + gPartNum);
+            break;
         case 'DATE':
-            let date = parts[1] * 4 / parts[2];
-            gDiv.currentTime = date;
-            gSound.currentTime = date;
+            console.log ("client receive", data);
+            let date = parts[1] * 4;
+            setTimeout( function(){ t1 = t2 = Date.now(); /*gSound.currentTime =*/ gVideo.currentTime = date }, getDelay(parts[2]));
             break;
-        case 'LAT':
-            if (parts[1] > gNetMaxLattency)
-                gNetMaxLattency = parts[1];
-            break;
-        }
+    }
 }
 
 function ready(div) {
     div.remove();
-    getLocalLatency();
     gReady = true;
     wsSend('PART ' + gPartNum);
 }
 
-function getDelay() {
-    return 1;
-    // return (gNetMaxLattency > gLocalLattency) ? (gNetMaxLattency - gLocalLattency) : 0;
+function getDelay(date) {
+    let n = date - gTime.now();
+console.log("Delay to", date, "->", n );
+    if (n < 0) console.log("late event detected: delay", n)
+    return (n > 0) ? n : 0;
 }
+
+function playAt (date) {
+    let delay = getDelay(date);
+    setTimeout( () => { t1 = t2 = Date.now(); /*gSound.play();*/ gVideo.play(); console.log("timeout diff",gTime.now()-date)}, delay);
+}
+
 
 var t1 = 0;
-function getLocalLatency () {
-    gDiv.addEventListener('playing', playLatency);
-    gDiv.pause();
-    t1 = Date.now();
-    gDiv.play();
+var t2 = 0;
+// function getLocalLatency () {
+//     gVideo.addEventListener('playing', videoLatency);
+//     gSound.addEventListener('playing', soundLatency);
+//     t1 = Date.now();
+//     gVideo.play();
+//     t2 = Date.now();
+//     gSound.play();
+// }
+
+function videoLatency() {
+    // if (!t1) return;
+    gVideoLatency = Date.now() - t1;
+    // gVideo.currentTime += gVideoLatency;
+    // gVideo.pause();
+    // gVideo.removeEventListener ('playing', videoLatency);
+    console.log ("Local video latency:", gVideoLatency);
+    // t1 = 0;
 }
 
-function playLatency() {
-    if (!t1) return;
-    gLocalLattency = Date.now() - t1;
-    gDiv.pause();
-    gDiv.removeEventListener ('playing', playLatency);
-    console.log ("Local play latency:", gLocalLattency);
-    wsSend('LAT ' + gLocalLattency);
-    t1 = 0;
+function soundLatency() {
+    // if (!t2) return;
+    gSoundLatency = Date.now() - t2;
+    console.log ("Local sound latency:", gSoundLatency);
+    // gSound.currentTime += gSoundLatency;
+    // gSound.pause();
+    // gSound.removeEventListener ('playing', soundLatency);
+    // t2 = 0;
 }
